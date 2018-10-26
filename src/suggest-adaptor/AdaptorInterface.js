1caece5bd71c80e4afcf2e13e77c45958ae9986b
yucong02 <moyuyc95@gmail.com>
2018-10-26 14:15:22 +0800

    docs: add AdaptorInterface comments

--- src/suggest-adaptor/AdaptorInterface.js
+++ src/suggest-adaptor/AdaptorInterface.js
@@ -8,7 +8,14 @@
 const parse = require('parse-github-url')
 
 /**
- * @class AdaptorInterface
+ * @typedef {{}} InputData
+ * @param namespace {string}
+ * @param matching {string}
+ */
+
+/**
+ * 适配器接口
+ * @class
  */
 class AdaptorInterface {
   get name() {
@@ -16,8 +23,10 @@ class AdaptorInterface {
   }
 
   /**
-   *
-   * @param data
+   * 扁平化，规范化 数据 （来自 fetch 方法）
+   * @param data {{}}
+   * @param inputData {InputData}
+   * @return {{title: string, issueURL?: string, issueId?: string, number?: string, searchText?: string} & *}
    */
   flattenIncomeData(data, inputData) {
     return {
@@ -30,12 +39,15 @@ class AdaptorInterface {
     }
   }
 
+  /**
+   *
+   * @param config {object} - cz-conventional-changelog-befe config
+   * @param pkg {object} - package.json
+   * @param gitUrl {string} - git remote url
+   */
   constructor(config = {}, pkg = {}, gitUrl = '') {
     if (typeof this.name !== 'string') {
-      throw new TypeError(
-        `Adaptor requires \`displayName\` (string) static property, but ${typeof this
-          .name}`
-      )
+      throw new TypeError(`Adaptor requires \`displayName\` (string) static property, but ${typeof this.name}`)
     }
     this.pkg = pkg
     this.config = config
@@ -49,20 +61,16 @@ class AdaptorInterface {
         this.gitUrlObj.path = this.gitUrlObj.pathname = RegExp.$1
       }
     } else if (pkg.repository) {
-      const repository =
-        typeof pkg.repository === 'string' ? pkg.repository : pkg.repository.url
+      const repository = typeof pkg.repository === 'string' ? pkg.repository : pkg.repository.url
       if (repository) {
         this.gitUrlObj = parse(repository)
       }
     }
 
     this.options = {
-      placeholder:
-        '{#:number?link} {[:type:]?align=center} {(:state:)?align=center} {title?w=50%}  {assignees}',
+      placeholder: '{#:number?link} {[:type:]?align=center} {(:state:)?align=center} {title?w=50%}  {assignees}',
       suggestEnabled: true,
-      ...(this.config && this.config[this.name.toLowerCase()]
-        ? this.config[this.name.toLowerCase()]
-        : this.config)
+      ...(this.config && this.config[this.name.toLowerCase()] ? this.config[this.name.toLowerCase()] : this.config)
     }
     this.data = (this.options || {}).data || {}
 
@@ -70,23 +78,46 @@ class AdaptorInterface {
     this.matchRegexps = ['^#(?<matching>.+)$']
   }
 
+  /**
+   * 判断 suggest adaptor 是否生效
+   * @public
+   * @return {boolean}
+   */
   isEnabled() {
     return this._isEnabled() && this.options.suggestEnabled
   }
 
+  /**
+   * @return {boolean}
+   * @protected
+   */
   _isEnabled() {
     return !!this.namespace
   }
 
+  /**
+   * 获取 suggest 列表
+   * @param inputData {InputData}
+   * @return {Array|Promise<Array>}
+   */
   fetch({ namespace, matching }) {
     return []
   }
 
+  /**
+   * 转换 commit message
+   * @param message {string}
+   * @return {string}
+   */
   transformCommitMessage(message) {
     return message
   }
 }
 
+/**
+ * name
+ * @type {string}
+ */
 AdaptorInterface.displayName = ''
 
 module.exports = AdaptorInterface
